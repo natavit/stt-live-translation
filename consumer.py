e1d1faaec83ff180d3a913962448fbc990cc8cd3
Natavit Rojcharoenpreeda <natavit@google.com>
2024-10-11 12:03:12 +0700

    feat: add pubsub consumer

---
+++ consumer.py
@@ -0,0 +1,32 @@
+import os
+from google.cloud import pubsub_v1
+
+project_id = "{REPLACE_WITH_PROJECT_ID}"
+pub_sub_topic = "live-translation"
+pub_sub_subscription = "live-translation-sub"
+
+location = "global"
+
+topic_name = 'projects/{project_id}/topics/{topic}'.format(
+    project_id=project_id,
+    topic=pub_sub_topic,
+)
+
+subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
+    project_id=project_id,
+    sub=pub_sub_subscription,
+)
+
+def callback(message):
+    print(message.data.decode("utf-8"))
+    message.ack()
+
+with pubsub_v1.SubscriberClient() as subscriber:
+    # subscriber.create_subscription(name=subscription_name, topic=topic_name)
+    
+    future = subscriber.subscribe(subscription_name, callback)
+    
+    try:
+        future.result()
+    except KeyboardInterrupt:
+        future.cancel()
